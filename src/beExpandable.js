d32a21f3b72fb1653bc092c9651689093af75945
ryym <ryym.64@gmail.com>
2016-01-30 17:42:32 +0900

    feat(beExpandable): Wrap component and pass props

--- src/beExpandable.js
+++ src/beExpandable.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
-export default function beExpandable(Component) {
+/**
+ * Wrap the given component to make it expandable.
+ * @param {React.Component} Component - The component to be wrapped.
+ * @param {object} options - The options.
+ * @return {React.Component} The wrapped component.
+ */
+export default function beExpandable(Component, options) {
+  if (! (options.width && options.height)) {
+    throw new Error('beExpandable: Please specify width and height');
+  }
   return React.createClass({
-
     render() {
+      if (! this.props.expander) {
+        throw new Error('beExpandable#render: Please set expander as a prop');
+      }
       return (
-        <Component />
+        <Component
+          {...this.props}
+          width={options.width}
+          height={options.height}
+        >
+        </Component>
       );
     }
-
   });
 }
--- test/unit/beExpandable.js
+++ test/unit/beExpandable.js
@@ -1,28 +1,97 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import assert from 'power-assert';
 import beExpandable from '$lib/beExpandable';
 
+/**
+ * A simple div component for tests.
+ */
+class Div extends React.Component {
+  render() {
+    return (
+      <div className="wrappee">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 describe('beExpandable', function() {
+  function renderExpandable(
+    Wrappee, props, options = { width: 100, height: 100 }
+  ) {
+    const Expandable = beExpandable(Wrappee, options);
+    return TestUtils.renderIntoDocument(<Expandable {...props} />);
+  }
 
   describe('options', () => {
-    it('requires width and height');
-  });
-
-  describe('props', () => {
-    it('requires expander passed from ExpandingAllower');
+    it('requires width and height', () => {
+      assert.throws(() => {
+        beExpandable(<div />, {});
+      });
+    });
   });
 
   describe('wrapping', () => {
+    const expander = {};
+
+    it('requires expander as a prop', () => {
+      assert.throws(() => {
+        renderExpandable(Div, {});
+      });
+    });
+
     it('does not add external div');
 
-    it('gives width and height to wrapped component');
+    it('renders a wrapped component', () => {
+      const expandable = renderExpandable(Div, { expander });
+      const wrappee = TestUtils.findRenderedComponentWithType(expandable, Div);
 
-    it('passes all the given props to wrapped component');
+      assert(wrappee);
+    });
 
-    it('passes its children to wrapped component');
+    it('gives width and height to wrapped component', () => {
+      const sizes = { width: 100, height: 100 };
+      const expandable = renderExpandable(Div, { expander }, sizes);
+      const div = TestUtils.findRenderedComponentWithType(expandable, Div);
+      const { width, height } = div.props;
 
-    it('renders expander element');
+      assert.deepEqual({ width, height }, sizes);
+    });
+
+    it('passes all the given props to wrapped component', () => {
+      const props = {
+        expander,
+        foo: 'foo',
+        bar: 'bar',
+        style: { color: 'red', margin: 0 },
+        onClick: e => { e.preventDefault(); }
+      };
+      const sizes = { width: 100, height: 100 };
+      const expandable = renderExpandable(Div, props, sizes);
+      const div = TestUtils.findRenderedComponentWithType(expandable, Div);
+
+      assert.deepEqual(div.props, Object.assign(props, sizes));
+    });
+
+    it('passes its children to wrapped component', () => {
+      const ExpandableDiv = beExpandable(Div, { width: 100, height: 100 });
+      const expandable = TestUtils.renderIntoDocument(
+        <ExpandableDiv expander={{}}>
+          <div className="child-div"></div>
+        </ExpandableDiv>
+      );
+
+      const child = TestUtils.findRenderedDOMComponentWithClass(expandable, 'child-div');
+      assert(child);
+    });
   });
 
   describe('expander element', () => {
+    it('renders expander element');
+
+    it('has a specified props');
+
     it('starts resizing on mouse down');
   });
 
