947c92964e3e942ce24c135ed00b2890a58ce41d
ryym <ryym.64@gmail.com>
2016-02-07 00:12:54 +0900

    chore(gulp): Declare lint config

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,27 @@ const TEST_HELPERS = [
   './test/mocha-env'
 ];
 
+const LINT_CONF = {
+  src: {},
+  test: {
+    envs: ['node', 'mocha'],
+    rules: {
+      'no-console': 1
+    }
+  },
+  gulpfile: {
+    envs: ['node'],
+    rules: {
+      'no-multi-spaces': 0,
+      'no-console': 0
+    }
+  },
+  eg: {
+    globals: ['require'],
+    envs: ['browser']
+  }
+};
+
 /**
  * Clear cached module to enable to re-require the module.
  * @param {string} path - The module path.
@@ -47,19 +68,20 @@ function runTests(pattern, options) {
 }
 
 /**
- * Lint sources by eslint. Throws an error if any problems are found.
+ * Lint sources by eslint. Throws an Error if any errors are found.
  * @param {string} pattern - The target file pattern.
- * @param {Object} options - The option passed to {@code eslint.CLIEngine}.
+ * @param {Object} configs - The config passed to {@code eslint.CLIEngine}.
+ * @param {boolean} strict - If true and there are one or more warnings,
+ *     an Error is thrown even if there is no errors.
  * @return {void}
  */
-function lintFiles(pattern, options) {
-  const linter = new eslint.CLIEngine(options);
-  const files = glob.sync(pattern);
-  const report = linter.executeOnFiles(files);
+function lintFiles(pattern, configs, strict) {
+  const linter = new eslint.CLIEngine(configs);
+  const report = linter.executeOnFiles([pattern]);
   const formatter = linter.getFormatter();
   console.log(formatter(report.results));
 
-  if (0 < report.errorCount || (options.strict && 0 < report.warningCount)) {
+  if (0 < report.errorCount || (strict && 0 < report.warningCount)) {
     throw new Error('eslint reports some problems.');
   }
 }
@@ -129,36 +151,19 @@ gulp.task('lint', [
 ]);
 
 gulp.task('lint:src', () => {
-  lintFiles(`${PATH.src}/*.js`, {
-    strict: true
-  });
+  lintFiles(`${PATH.src}/*.js`, LINT_CONF.src, true);
 });
 
 gulp.task('lint:test', () => {
-  lintFiles(`${PATH.test}/**/*.js`, {
-    envs: ['node', 'mocha'],
-    rules: {
-      'no-console': 1
-    }
-  });
+  lintFiles(`${PATH.test}/**/*.js`, LINT_CONF.test, true);
 });
 
 gulp.task('lint:gulpfile', () => {
-  lintFiles('./gulpfile.js', {
-    envs: ['node'],
-    rules: {
-      'no-multi-spaces': 0,
-      'no-console': 0
-    }
-  });
+  lintFiles('./gulpfile.js', LINT_CONF.gulpfile, true);
 });
 
 gulp.task('lint:eg', () => {
-  lintFiles('./example/src/**/*.js', {
-    strict: true,
-    globals: ['require'],
-    envs: ['browser']
-  });
+  lintFiles('./example/src', LINT_CONF.eg, true);
 });
 
 gulp.task('lint:watch', ['lint'], () => {
