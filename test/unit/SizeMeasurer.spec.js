e2d84a1e685f940f826830da1bfc04f4a8717456
ryym <ryym.64@gmail.com>
2016-02-14 11:39:52 +0900

    test(SizeMeasurer): Rewirte parameterized test by mocha-each

--- test/unit/SizeMeasurer.spec.js
+++ test/unit/SizeMeasurer.spec.js
@@ -1,28 +1,15 @@
+import forEach from 'mocha-each';
 import assert from 'power-assert';
 import {
   measures,
   normalizeDirections
 } from '$src/SizeMeasurer';
 
-/**
- * Execute parametarized test.
- */
-function itEach(testName, params, testBody) {
-  const makeTestName = typeof testName === 'function'
-    ? testName : () => testName;
-
-  params.forEach(param => {
-    it(makeTestName(param), () => {
-      testBody(...param);
-    });
-  });
-}
-
 describe('measures', () => {
   function testEach(calculator, params) {
-    itEach(
-      param => `handles ${JSON.stringify(param[0])}`,
-      params, (args, out) => {
+    forEach(params).it(
+      param => `handles ${JSON.stringify(param)}`,
+      (args, out) => {
         assert.equal(calculator(...args), out);
       }
     );
@@ -69,51 +56,49 @@ describe('measures', () => {
 });
 
 describe('normalizeDirections()', () => {
-  itEach(
-    params => `handles ${JSON.stringify(params[0])}`,
-    [
-      [['top'], {
-        dirX: 'asIs', dirY: 'top'
-      }],
-      [['bottom'], {
-        dirX: 'asIs', dirY: 'bottom'
-      }],
-      [['left'], {
-        dirX: 'left', dirY: 'asIs'
-      }],
-      [['right'], {
-        dirX: 'right', dirY: 'asIs'
-      }],
-      [['top left'], {
-        dirX: 'left', dirY: 'top'
-      }],
-      [['bottom right'], {
-        dirX: 'right', dirY: 'bottom'
-      }],
-      [['left bottom'], {
-        dirX: 'left', dirY: 'bottom'
-      }],
-      [['left bottom right'], {
-        dirX: 'left', dirY: 'bottom'
-      }]
-    ],
-    ([directions], expected) => {
+  forEach([
+    ['top', {
+      dirX: 'asIs', dirY: 'top'
+    }],
+    ['bottom', {
+      dirX: 'asIs', dirY: 'bottom'
+    }],
+    ['left', {
+      dirX: 'left', dirY: 'asIs'
+    }],
+    ['right', {
+      dirX: 'right', dirY: 'asIs'
+    }],
+    ['top left', {
+      dirX: 'left', dirY: 'top'
+    }],
+    ['bottom right', {
+      dirX: 'right', dirY: 'bottom'
+    }],
+    ['left bottom', {
+      dirX: 'left', dirY: 'bottom'
+    }],
+    ['left bottom right', {
+      dirX: 'left', dirY: 'bottom'
+    }]
+  ])
+  .it(
+    params => `handles ${JSON.stringify(params)}`,
+    (directions, expected) => {
       const actual = normalizeDirections(directions);
       assert.deepEqual(actual, expected);
     }
   );
 
   context('with invalid directions', () => {
-    itEach(
-      param => `throws an error for ${JSON.stringify(param)}`,
-      [
-        ['top bottom'],
-        ['top top'],
-        ['left right'],
-        ['right right']
-      ], arg => {
-        assert.throws(() => normalizeDirections(arg));
-      }
-    );
+    forEach([
+      ['top bottom'],
+      ['top top'],
+      ['left right'],
+      ['right right']
+    ])
+    .it('throws an error for %j', arg => {
+      assert.throws(() => normalizeDirections(arg));
+    });
   });
 });
