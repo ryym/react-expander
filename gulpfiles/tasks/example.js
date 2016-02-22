7c7dce997660d6599524a460482c1a4f2ea62dc0
ryym <ryym.64@gmail.com>
2016-02-22 23:40:24 +0900

    chore(gulp): Use othernpm to run example tasks

--- gulpfiles/tasks/example.js
+++ gulpfiles/tasks/example.js
@@ -1,34 +1,11 @@
 const gulp = require('gulp');
-const cp   = require('child_process');
-const $    = require('./_shared');
-
-/**
- * Change the current directory to
- * the example directory.
- * @return {void}
- */
-function gotoExampleDir() {
-  process.chdir($.PATH.eg);
-}
-
-/**
- * Run the specified npm script.
- * @param {string} scriptName
- * @return {Object} A process object returned from `child_process.spawn`.
- */
-function runNpmScript(scriptName) {
-  return cp.spawn('npm', [ 'run', scriptName ], {
-    stdio: 'inherit'
-  });
-}
+const $ = require('./_shared');
+const onpm = require('othernpm')($.PATH.eg);
 
 gulp.task('eg:dev', () => {
-  gotoExampleDir();
-  runNpmScript('dev');
+  onpm('run dev');
 });
 
 gulp.task('eg:build', done => {
-  gotoExampleDir();
-  const build = runNpmScript('build');
-  build.on('exit', done);
+  onpm('run build').on('exit', done);
 });
