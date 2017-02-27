5350006fc6ab4836b19bb7f7365e4758a6601229
ryym <ryym.64@gmail.com>
2017-02-28 00:21:17 +0900

    WIP [ci skip]

---
+++ examples/hello-world/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlTemplatePlugin = require('html-webpack-plugin');
+
+const PATH = {
+  root: path.join(__dirname),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: path.join(PATH.root, 'index.js'),
+
+  output: {
+    path: PATH.build,
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        include: path.join(PATH.root, 'index.js'),
+      },
+    ]
+  },
+
+  devServer: {
+    contentBase: PATH.build,
+  },
+
+  plugins: [
+    new HtmlTemplatePlugin({
+      template: path.join(PATH.root, 'index.template.html'),
+      inject: 'body'
+    }),
+  ]
+};
+
